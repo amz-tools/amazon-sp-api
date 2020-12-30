ac5c9ea6186bf71cbfa293fafaef6ee2ae5446a0
Nick O'Hara <57359901+nohara-embark@users.noreply.github.com>
2020-12-30 11:31:35 -0500

    Testing package types

--- lib/typings/SellingPartner.d.ts
+++ lib/typings/SellingPartner.d.ts
@@ -1,15 +1,4 @@
 declare module "nick-testing-amazon-sp-api" {
-  class SellingPartner {
-    constructor(config: Config): void;
-
-    async refreshAccessToken(): void;
-    async refreshRoleCredentials(): void;
-
-    async callAPI(test: string): void;
-  }
-
-  export = SellingPartner;
-
   export interface RoleCredentials {
     id?: string;
     secret?: string;
@@ -29,4 +18,57 @@ declare module "nick-testing-amazon-sp-api" {
     role_credentials: RoleCredentials;
     options: Options;
   }
+
+  class SellingPartner {
+    constructor(config: Config): void;
+    async refreshAccessToken(): void;
+    async refreshRoleCredentials(): void;
+    async callAPI<T extends Operation>(req_params: ReqParams<T>): ObjectType<T>;
+  }
+
+  const testing = new SellingPartner().callAPI<'getAuthorizationCode'>({
+    operation: 'getAuthorizationCode',
+    query: {
+      sellingPartnerId: 'string',
+    }
+  });
+  console.log(testing);
+
+  type Operation = "getAuthorizationCode" | "x";
+
+  type ObjectType<T> = T extends "getAuthorizationCode" ? GetAuthorizationCodeResponse
+      : T extends "x" ? SomethingElse
+      : never;
+
+  export interface ReqParams<T> {
+    operation: T;
+    path?: any;
+    query?: T extends "getAuthorizationCode" ? GetAuthorizationCodeQuery
+      : T extends "something else" ? SomethingElse
+      : never;
+    body?: any;
+  }
+
+  interface Errors {
+    code: string;
+    message: string;
+    details?: string;
+  }
+
+  interface SomethingElse {}
+
+  interface GetAuthorizationCodeQuery {
+    sellingPartnerId: string;
+    developerId: string;
+    mwsAuthToken: string;
+  }
+
+  interface GetAuthorizationCodeResponse {
+    payload?: {
+      authorizationCode: string;
+    };
+    errors?: Errors;
+  }
+
+  export = SellingPartner;
 }
