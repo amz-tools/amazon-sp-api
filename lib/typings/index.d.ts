a6b1cd6d16f6fec7b16afcc601ad35d704777398
Nick O'Hara <57359901+nohara-embark@users.noreply.github.com>
2021-01-04 10:12:22 -0500

    Add fba small and light api

--- lib/typings/index.d.ts
+++ lib/typings/index.d.ts
@@ -1,26 +1,45 @@
+import {
+  DeleteSmallAndLightEnrollmentBySellerSKUPath,
+  DeleteSmallAndLightEnrollmentBySellerSKUQuery,
+  GetSmallAndLightEligibilityBySellerSKUPath,
+  GetSmallAndLightEligibilityBySellerSKUQuery,
+  GetSmallAndLightEligibilityBySellerSKUResponse,
+  GetSmallAndLightEnrollmentBySellerSKUPath,
+  GetSmallAndLightEnrollmentBySellerSKUQuery,
+  GetSmallAndLightEnrollmentBySellerSKUResponse,
+  PutSmallAndLightEnrollmentBySellerSKUPath,
+  PutSmallAndLightEnrollmentBySellerSKUQuery,
+  PutSmallAndLightEnrollmentBySellerSKUResponse,
+} from "./operations/fba-small-and-light";
+import {
+  GetAuthorizationCodeQuery,
+  GetAuthorizationCodeResponse,
+} from "./operations/authorization";
+import {
+  GetCatalogItemPath,
+  GetCatalogItemQuery,
+  GetCatalogItemResponse,
+  ListCatalogCategoriesQuery,
+  ListCatalogCategoriesResponse,
+  ListCatalogItemsQuery,
+  ListCatalogItemsResponse,
+} from "./operations/catalog-items";
+import {
+  GetInventorySummariesQuery,
+  GetInventorySummariesResponse,
+} from "./operations/fba-inventory";
+import {
+  GetItemEligibilityPreviewQuery,
+  GetItemEligibilityPreviewResponse,
+} from "./operations/fba-inbound-eligibility";
+
+import { Config } from "./base-types";
+
 declare module "nick-testing-amazon-sp-api" {
   import {
-    ListCatalogItemsResponse,
-    ListCatalogItemsQuery,
-    GetCatalogItemQuery,
-    GetCatalogItemPath,
-    GetCatalogItemResponse,
-    ListCatalogCategoriesQuery,
-    ListCatalogCategoriesResponse,
-  } from "./operations/catalog-items";
-  import {
-    GetAuthorizationCodeQuery,
-    GetAuthorizationCodeResponse,
-  } from "./operations/authorization";
-  import { Config } from "./base-types";
-  import {
-    GetItemEligibilityPreviewQuery,
-    GetItemEligibilityPreviewResponse,
-  } from "./operations/fba-inbound-eligibility";
-  import {
-    GetInventorySummariesQuery,
-    GetInventorySummariesResponse,
-  } from "./operations/fba-inventory";
+    GetSmallAndLightFeePreviewBody,
+    GetSmallAndLightFeePreviewResponse,
+  } from "./operations/fba-small-and-light";
 
   class SellingPartner {
     constructor(config: Config): void;
@@ -49,6 +68,14 @@ declare module "nick-testing-amazon-sp-api" {
     ? GetItemEligibilityPreviewResponse
     : TOperation extends "getInventorySummaries"
     ? GetInventorySummariesResponse
+    : TOperation extends "GetSmallAndLightEnrollmentBySellerSKUResponse"
+    ? GetSmallAndLightEnrollmentBySellerSKUResponse
+    : TOperation extends "putSmallAndLightEnrollmentBySellerSKU"
+    ? PutSmallAndLightEnrollmentBySellerSKUResponse
+    : TOperation extends "getSmallAndLightEligibilityBySellerSKU"
+    ? GetSmallAndLightEligibilityBySellerSKUResponse
+    : TOperation extends "getSmallAndLightFeePreview"
+    ? GetSmallAndLightFeePreviewResponse
     : never;
 
   type QueryType<
@@ -65,13 +92,35 @@ declare module "nick-testing-amazon-sp-api" {
     ? GetItemEligibilityPreviewQuery
     : TOperation extends "getInventorySummaries"
     ? GetInventorySummariesQuery
+    : TOperation extends "getSmallAndLightEnrollmentBySellerSKU"
+    ? GetSmallAndLightEnrollmentBySellerSKUQuery
+    : TOperation extends "putSmallAndLightEnrollmentBySellerSKU"
+    ? PutSmallAndLightEnrollmentBySellerSKUQuery
+    : TOperation extends "deleteSmallAndLightEnrollmentBySellerSKU"
+    ? DeleteSmallAndLightEnrollmentBySellerSKUQuery
+    : TOperation extends "getSmallAndLightEligibilityBySellerSKU"
+    ? GetSmallAndLightEligibilityBySellerSKUQuery
     : never;
 
   type PathType<
     TOperation extends Operation
-  > = TOperation extends "getCatalogItem" ? GetCatalogItemPath : never;
+  > = TOperation extends "getCatalogItem"
+    ? GetCatalogItemPath
+    : TOperation extends "getSmallAndLightEnrollmentBySellerSKU"
+    ? GetSmallAndLightEnrollmentBySellerSKUPath
+    : TOperation extends "putSmallAndLightEnrollmentBySellerSKU"
+    ? PutSmallAndLightEnrollmentBySellerSKUPath
+    : TOperation extends "deleteSmallAndLightEnrollmentBySellerSKU"
+    ? DeleteSmallAndLightEnrollmentBySellerSKUPath
+    : TOperation extends "getSmallAndLightEligibilityBySellerSKU"
+    ? GetSmallAndLightEligibilityBySellerSKUPath
+    : never;
 
-  type BodyType<TOperation extends Operation> = never;
+  type BodyType<
+    TOperation extends Operation
+  > = TOperation extends "getSmallAndLightFeePreview"
+    ? GetSmallAndLightFeePreviewBody
+    : never;
 
   export interface ReqParams<TOperation extends Operation> {
     operation: TOperation;
