19393a2f89d80b4205bc5e4232d9b4024683500b
amz-tools <info@amz.tools>
2021-02-17 00:23:09 +0100

    Added tests for aplus content operations

---
+++ test/specs/operations/aplusContent.spec.js
@@ -0,0 +1,72 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('aplusContent', async function(){
+
+  let content_reference_key;
+
+	it('should return assigned aplus content documents', async function(){
+		let res = await this.sellingPartner.callAPI({
+			operation:'searchContentDocuments',
+			query:{
+      	marketplaceId:this.config.marketplace_id
+      }
+    });
+    expect(res).to.be.a('object');
+    expect(res.contentMetadataRecords).to.be.a('array');
+
+    if (res.contentMetadataRecords[0]){
+      content_reference_key = res.contentMetadataRecords[0].contentReferenceKey;
+    }
+	});
+
+  it('should return a plus content document for content reference key', async function(){
+    if (content_reference_key){
+      let res = await this.sellingPartner.callAPI({
+        operation:'getContentDocument',
+        path:{
+          contentReferenceKey:content_reference_key
+        },
+        query:{
+          marketplaceId:this.config.marketplace_id,
+          includedDataSet:['CONTENTS', 'METADATA']
+        }
+      });
+      expect(res).to.be.a('object');
+      expect(res.contentRecord).to.be.a('object');
+    } else {
+      this.skip();
+    }
+  });
+
+  it('should return a list of asins related to aplus content for content reference key', async function(){
+    if (content_reference_key){
+      let res = await this.sellingPartner.callAPI({
+        operation:'listContentDocumentAsinRelations',
+        path:{
+          contentReferenceKey:content_reference_key
+        },
+        query:{
+          marketplaceId:this.config.marketplace_id
+        }
+      });
+      expect(res).to.be.a('object');
+      expect(res.asinMetadataSet).to.be.a('array');
+    } else {
+      this.skip();
+    }
+  });
+
+  it('should return a list of asins related to aplus content for content reference key', async function(){
+    let res = await this.sellingPartner.callAPI({
+      operation:'searchContentPublishRecords',
+      query:{
+        marketplaceId:this.config.marketplace_id,
+        asin:this.config.asin
+      }
+    });
+    expect(res).to.be.a('object');
+    expect(res.publishRecordList).to.be.a('array');
+  });
+
+});
