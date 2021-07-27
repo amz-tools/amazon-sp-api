2138bd0ef1f3ce756df05f81662af235220329a3
amz-tools <info@amz.tools>
2021-07-27 21:44:39 +0200

    Readded aplus tests

--- test/specs/operations/aplusContent.spec.js
+++ test/specs/operations/aplusContent.spec.js
@@ -3,78 +3,76 @@ const expect = chai.expect;
 
 const endpoint = 'aplusContent';
 
-// Tests currently commented out as aplus content endpoint is not working as expected anymore --> see "Known Issues" in readme
-
 describe(endpoint, async function(){
 
-  // let content_reference_key;
+  let content_reference_key;
 
- //  it('should return assigned aplus content documents', async function(){
-	// 	let res = await this.sellingPartner.callAPI({
-	// 		operation:'searchContentDocuments',
- //      endpoint:endpoint,
-	// 		query:{
- //      	marketplaceId:this.config.marketplace_id
- //      }
- //    });
- //    expect(res).to.be.a('object');
- //    expect(res.contentMetadataRecords).to.be.a('array');
+  it('should return assigned aplus content documents', async function(){
+		let res = await this.sellingPartner.callAPI({
+			operation:'searchContentDocuments',
+      endpoint:endpoint,
+			query:{
+      	marketplaceId:this.config.marketplace_id
+      }
+    });
+    expect(res).to.be.a('object');
+    expect(res.contentMetadataRecords).to.be.a('array');
 
- //    if (res.contentMetadataRecords[0]){
- //      content_reference_key = res.contentMetadataRecords[0].contentReferenceKey;
- //    }
-  // });
+    if (res.contentMetadataRecords[0]){
+      content_reference_key = res.contentMetadataRecords[0].contentReferenceKey;
+    }
+  });
 
- //  it('should return a plus content document for content reference key', async function(){
- //    if (content_reference_key){
- //      let res = await this.sellingPartner.callAPI({
- //        operation:'getContentDocument',
- //        endpoint:endpoint,
- //        path:{
- //          contentReferenceKey:content_reference_key
- //        },
- //        query:{
- //          marketplaceId:this.config.marketplace_id,
- //          includedDataSet:['CONTENTS', 'METADATA']
- //        }
- //      });
- //      expect(res).to.be.a('object');
- //      expect(res.contentRecord).to.be.a('object');
- //    } else {
- //      this.skip();
- //    }
- //  });
+  it('should return a plus content document for content reference key', async function(){
+    if (content_reference_key){
+      let res = await this.sellingPartner.callAPI({
+        operation:'getContentDocument',
+        endpoint:endpoint,
+        path:{
+          contentReferenceKey:content_reference_key
+        },
+        query:{
+          marketplaceId:this.config.marketplace_id,
+          includedDataSet:['CONTENTS', 'METADATA']
+        }
+      });
+      expect(res).to.be.a('object');
+      expect(res.contentRecord).to.be.a('object');
+    } else {
+      this.skip();
+    }
+  });
 
- //  it('should return a list of asins related to aplus content for content reference key', async function(){
- //    if (content_reference_key){
- //      let res = await this.sellingPartner.callAPI({
- //        operation:'listContentDocumentAsinRelations',
- //        endpoint:endpoint,
- //        path:{
- //          contentReferenceKey:content_reference_key
- //        },
- //        query:{
- //          marketplaceId:this.config.marketplace_id
- //        }
- //      });
- //      expect(res).to.be.a('object');
- //      expect(res.asinMetadataSet).to.be.a('array');
- //    } else {
- //      this.skip();
- //    }
- //  });
+  it('should return a list of asins related to aplus content for content reference key', async function(){
+    if (content_reference_key){
+      let res = await this.sellingPartner.callAPI({
+        operation:'listContentDocumentAsinRelations',
+        endpoint:endpoint,
+        path:{
+          contentReferenceKey:content_reference_key
+        },
+        query:{
+          marketplaceId:this.config.marketplace_id
+        }
+      });
+      expect(res).to.be.a('object');
+      expect(res.asinMetadataSet).to.be.a('array');
+    } else {
+      this.skip();
+    }
+  });
 
- //  it('should return a list of a+ content publishing records for asin', async function(){
- //    let res = await this.sellingPartner.callAPI({
- //      operation:'searchContentPublishRecords',
- //      endpoint:endpoint,
- //      query:{
- //        marketplaceId:this.config.marketplace_id,
- //        asin:this.config.asin
- //      }
- //    });
- //    expect(res).to.be.a('object');
- //    expect(res.publishRecordList).to.be.a('array');
- //  });
+  it('should return a list of a+ content publishing records for asin', async function(){
+    let res = await this.sellingPartner.callAPI({
+      operation:'searchContentPublishRecords',
+      endpoint:endpoint,
+      query:{
+        marketplaceId:this.config.marketplace_id,
+        asin:this.config.asin
+      }
+    });
+    expect(res).to.be.a('object');
+    expect(res.publishRecordList).to.be.a('array');
+  });
 
 });
