74c72d836e8badb3c47580f493f14e2758ebc70f
amz-tools <info@amz.tools>
2021-10-12 23:25:19 +0200

    Changed tokens test spec

--- test/specs/operations/tokens.spec.js
+++ test/specs/operations/tokens.spec.js
@@ -5,23 +5,25 @@ const endpoint = 'tokens';
 
 describe(endpoint, async function(){
 
-  it('should return a restricted data token', async function(){
-    if (this.config.order_id){
+  it('should return a restricted data token or an access denied error', async function(){
+    try {
       let res = await this.sellingPartner.callAPI({
         operation:'createRestrictedDataToken',
         endpoint:endpoint,
         body:{
           restrictedResources:[{
-            path:'/orders/v0/orders/' + this.config.order_id + '/address',
-            method:'GET'
+            path:'/orders/v0/orders',
+            method:'GET',
+            dataElements:['buyerInfo', 'shippingAddress']
           }]
         }
       });
       expect(res).to.be.a('object');
       expect(res.restrictedDataToken).to.be.a('string');
       expect(res.expiresIn).to.be.a('number');
-    } else {
-      this.skip();
+    } catch(e){
+      expect(e).to.be.an('error');
+      expect(e.message).to.equal('Application does not have access to one or more requested data elements: [buyerInfo, shippingAddress]');
     }
   });
 
