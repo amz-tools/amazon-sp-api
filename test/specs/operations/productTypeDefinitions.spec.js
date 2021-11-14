90d1ea8cb8c2a36177c19f45b019528c26025582
amz-tools <info@amz.tools>
2021-11-14 23:58:21 +0100

    Renamed productTypeDefinitions endpoint

--- test/specs/operations/productTypeDefinitions.spec.js
+++ test/specs/operations/productTypeDefinitions.spec.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const endpoint = 'productTypesDefinitions';
+const endpoint = 'productTypeDefinitions';
 
 describe(endpoint, async function(){
 
