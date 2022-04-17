624799fa0a1052e13464b5190d337b25e7e008e7
amz-tools <info@amz.tools>
2022-04-17 19:53:00 +0200

    Added test spec stub for new endpoints easyShip and vendorDirectFulfillmentSandboxTestData

---
+++ test/specs/operations/easyShip.spec.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const endpoint = 'easyShip';
+
+describe(endpoint, async function(){
+
+  // TODO: Define tests
+
+});
---
+++ test/specs/operations/vendorDirectFulfillmentSandboxTestData.spec.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const endpoint = 'vendorDirectFulfillmentSandboxTestData';
+
+describe(endpoint, async function(){
+
+  // TODO: Define tests
+
+});
