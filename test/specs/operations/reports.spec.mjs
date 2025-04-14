391ae64404de2fef0365ecddf2821340174ee1b7
amz-tools <info@amz.tools>
2025-04-14 13:47:59 +0200

    Added test for downloadReportStream

--- test/specs/operations/reports.spec.mjs
+++ test/specs/operations/reports.spec.mjs
@@ -25,84 +25,6 @@ describe(endpoint, async function () {
     }
   });
 
-  it("should (by stream api) return downloaded the content of open listings inventory report document", async function () {
-    this.timeout(100000);
-    try {
-      const req_params = {
-        body: {
-          reportType: "GET_FLAT_FILE_OPEN_LISTINGS_DATA",
-          marketplaceIds: [this.config.marketplace_id]
-        },
-        cancel_after: 3,
-        interval: 10000
-      };
-      const { reportId: report_id } = await this.sellingPartner.callAPI({
-        operation: "reports.createReport",
-        body: req_params.body,
-        options: {
-          ...(req_params.version ? { version: req_params.version } : {})
-        }
-      });
-      console.log("created the report", report_id);
-      // wait a sensitive amount of time for report to be ready
-      await new Promise((s, r) => setTimeout(() => s(1), 60000));
-      // try 3 times, until success or failure
-      let report_document_id = await (async () => {
-        let report_doc_id = 0;
-        let tries = 0;
-        while (!report_doc_id || tries < 3) {
-          const res = await this.sellingPartner.callAPI({
-            operation: "reports.getReport",
-            path: {
-              reportId: report_id
-            },
-            options: {
-              ...(req_params.version ? { version: req_params.version } : {})
-            }
-          });
-          // TODO: When processingStatus is "CANCELLED" or "FATAL" we stay in an endless loop here
-          if (res.processingStatus === "DONE") {
-            return res.reportDocumentId;
-          }
-          await new Promise((s, r) => setTimeout(() => s(1), 10000));
-        }
-      })();
-      const report_document = await this.sellingPartner.callAPI({
-        operation: "reports.getReportDocument",
-        path: {
-          reportDocumentId: report_document_id
-        },
-        options: {
-          ...(req_params.version ? { version: req_params.version } : {})
-        }
-      });
-      const docStream =
-        await this.sellingPartner.downloadStream(report_document);
-      // expect(res).to.be.a("array");
-      let data = "";
-      docStream.on("data", (chunk) => {
-        if (data.length < 1000) {
-          data += chunk.toString();
-        }
-      });
-      await new Promise((resolve, reject) => {
-        docStream.on("end", () => {
-          // TODO: This fails in case reporting language in Sellercentral is not English
-          // --> GET_FLAT_FILE_OPEN_LISTINGS_DATA report always returns field names in reporting language
-          // --> i.e. if language is german, the field name for price is not "Price" but "Preis"
-          // --> try to do another validity check or maybe use another report for testing
-          expect(data).to.contain("Price");
-          expect(data).to.contain("sku");
-          resolve();
-        });
-      });
-    } catch (e) {
-      console.log("ERROR DURRRRRING STREAM", e);
-      expect(e).to.be.an("error");
-      expect(e.code).to.equal("REPORT_PROCESSING_CANCELLED_MANUALLY");
-    }
-  });
-
   it("should return report details for open listings inventory reports", async function () {
     let res = await this.sellingPartner.callAPI({
       operation: "getReports",
@@ -207,5 +129,29 @@ describe(endpoint, async function () {
     } else {
       this.skip();
     }
+  });
+
+  it("should return streamed decrypted content of open listings inventory report document", async function () {
+    this.timeout(120000);
+    let resStream = await this.sellingPartner.downloadReportStream({
+      body: {
+        reportType: "GET_V2_SELLER_PERFORMANCE_REPORT",
+        marketplaceIds: [this.config.marketplace_id]
+      },
+      download: {
+        unzip: true
+      }
+    });
+    let data = "";
+    resStream.on("data", (chunk) => {
+      data += chunk.toString();
+    });
+    resStream.on("error", async (err) => {});
+    resStream.on("end", () => {
+      data = JSON.parse(data);
+      expect(data).to.be.a("object");
+      expect(data.accountStatuses).to.be.a("array");
+      expect(data.performanceMetrics).to.be.a("array");
+    });
   });
 });
